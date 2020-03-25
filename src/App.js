51dd6f43f3956e585040ed2f20da57ea239275f6
Enzo <enzoherrera123@gmail.com>
2020-03-24 22:18:38 -0700

    Added new files

--- src/App.js
+++ src/App.js
@@ -1,28 +1,69 @@
-import React, { Component } from "react";
-
+import React, { Component, ReactPropTypes } from "react";
 
 import "./App.css";
 
+//App creates the Button Components
 class App extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
 
+    this.state = {
+      isPlaying: false,
+    };
+
+    this._playSound = this._playSound.bind(this);
+    this._onTransitionEnd = this._onTransitionEnd.bind(this);
   }
 
-  
+  //What Happens when Component is mounted
+  componentDidMount() {
+    window.addEventListener("keydown", this._playSound);
+    this.Audio = new Audio(this.props.source);
+  }
+
+  //Removes EL before component is destroyed.
+  componentWillUnmount() {
+    window.removeEventListener("keydown", this._playSound);
+  }
+
+  _playSound(event) {
+    if (event.keyCode === this.props.code || event.type === "click") {
+      this.setState({ isPlaying: true });
+      this.Audio.currentTime = 0;
+      this.Audio.play();
+    }
+  }
+
+  _onTransitionEnd(event) {
+    this.setState({ isPlaying: false });
+  }
 
   render() {
-
-
+    const { style, bigText, smallText, className } = this.props;
+    const isPlaying = this.state.isPlaying
+      ? `${className} isPlaying`
+      : `${className}`;
     return (
-      <div>
-        <h1>DrumKit Project</h1>
-        
-      </div>
+      <button
+        onClick={this._playSound}
+        onTransitionEnd={this._onTransitionEnd}
+        className={isPlaying}
+        style={style}
+      >
+        <kbd>{smallText}</kbd>
+        <span>{bigText}</span>
+      </button>
     );
   }
 }
 
+DrumKit.ReactPropTypes = {
+  className: PropTypes.string,
+  style: PropTypes.object,
+  bigText: PropTypes.string.isRequired,
+  smallText: PropTypes.string.isRequired,
+  code: PropTypes.number.isRequired,
+  source: PropTypes.string.isRequired
+};
+
 export default App;
-
-
--- src/index.js
+++ src/index.js
@@ -3,7 +3,18 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-import * as serviceWorker from "./serviceWorker";
+//Imports the Sound
+import clap from '../sounds/clap.wav';
+import hihat from '../sounds/hihat.wav';
+import kick from '../sounds/kick.wav';
+import openhat from '../sounds/openhat.wav';
+import boom from '../sounds/boom.wav';
+import ride from '../sounds/ride.wav';
+import snare from '../sounds/snare.wav';
+import tom from '../sounds/tom.wav';
+import tink from '../sounds/tink.wav';
+
+
 
 ReactDOM.render(
   <React.StrictMode>
